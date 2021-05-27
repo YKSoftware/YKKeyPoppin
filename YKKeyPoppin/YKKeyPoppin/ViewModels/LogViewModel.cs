911eefbb4097a8559b7e41be13b0cb36192cff2a
Yujiro Kato <yu-kato@shibuya.co.jp>
2021-05-27 17:54:31 +0900

    ログ画面にグラフを追加。

--- YKKeyPoppin/YKKeyPoppin/ViewModels/LogViewModel.cs
+++ YKKeyPoppin/YKKeyPoppin/ViewModels/LogViewModel.cs
@@ -1,12 +1,38 @@
 ﻿namespace YKKeyPoppin.ViewModels
 {
     using System.Collections.Generic;
+    using System.Linq;
     using YKKeyPoppin.Models;
     using YKToolkit.Bindings;
+    using YKToolkit.Controls;
     using YKToolkit.Helpers;
 
     internal class LogViewModel : NotificationObject
     {
+        public LogViewModel()
+        {
+            var xValues = this.AllLog.Select(x => (double)x.DateTime.ToBinary());
+            var min = xValues.Min();
+            var max = xValues.Max();
+            this.XAxisSettings = new AxisSettings()
+            {
+                Minimum = min,
+                Maximum = max,
+                MajorStep = (max - min) / 10.0,
+                GridLabelVisibility = System.Windows.Visibility.Collapsed,
+            };
+
+            var yValues = this.AllLog.Select(x => (double)x.TotalHits);
+            min = ((long)yValues.Min() / 100) * 100;
+            max = ((long)yValues.Max() / 100) * 100 + 100;
+            this.YAxisSettings = new AxisSettings()
+            {
+                Minimum = min,
+                Maximum = max,
+                MajorStep = (max - min) / 10.0,
+            };
+        }
+
         /// <summary>
         /// タイトルを取得します。
         /// </summary>
@@ -16,6 +42,11 @@
         /// 全ログデータを取得します。
         /// </summary>
         public IEnumerable<KeyLogData> AllLog { get { return KeyCollector.Current.AllCollections; } }
+
+        public AxisSettings XAxisSettings { get; private set; }
+        public AxisSettings YAxisSettings { get; private set; }
+        public double[] XData { get { return this.AllLog.Select(x => (double)x.DateTime.ToBinary()).ToArray(); } }
+        public double[] YData { get { return this.AllLog.Select(x => (double)x.TotalHits).ToArray(); } }
 
         /// <summary>
         /// プロダクト情報
